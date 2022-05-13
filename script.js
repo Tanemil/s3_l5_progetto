4c0dda75ed48f1e44d3b4c49718ab08aac40e39e
emanuel <emanuel1999emy@gmail.com>
2022-05-13 11:45:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let stringa_risultato = document.getElementsByTagName('div')[0].getElementsByTagName('p')[0]
+let risultato = 0
+let numero
+
+function aggiungi_0(){
+    stringa_risultato.innerText += '0'
+    numero += '0'
+}
+function aggiungi_1(){
+    stringa_risultato.innerText += '1'
+    numero += '1'
+}
+function aggiungi_2(){
+    stringa_risultato.innerText += '2'
+    numero += '2'
+}
+function aggiungi_3(){
+    stringa_risultato.innerText += '3'
+    numero += '3'
+}
+function aggiungi_4(){
+    stringa_risultato.innerText += '4'
+    numero += '4'
+}
+function aggiungi_5(){
+    stringa_risultato.innerText += '5'
+    numero += '5'
+}
+function aggiungi_6(){
+    stringa_risultato.innerText += '6'
+    numero += '6'
+}
+function aggiungi_7(){
+    stringa_risultato.innerText += '7'
+    numero += '7'
+}
+function aggiungi_8(){
+    stringa_risultato.innerText += '8'
+    numero += '8'
+}
+function aggiungi_9(){
+    stringa_risultato.innerText += '9'
+    numero += '9'
+}
+function aggiungi_diviso(){
+    stringa_risultato.innerText += '/'
+}
+function aggiungi_per(){
+    stringa_risultato.innerText += '*'
+}
+function aggiungi_meno(){
+    stringa_risultato.innerText += '-'
+}
+function aggiungi_piu(){
+    stringa_risultato.innerText += '+'
+}
+function cancella(){
+    stringa_risultato.innerText += '9'
+}
+function aggiungi_punto(){
+    stringa_risultato.innerText += '.'
+}
