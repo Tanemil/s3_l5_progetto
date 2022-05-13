b971c6d7c9899ad0de8115312e5ff55d343fdde0
emanuel <emanuel1999emy@gmail.com>
2022-05-13 15:54:11 +0200

    commit

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+
+// stringa contenente i valori del display
 let stringa_risultato = document.getElementsByTagName('div')[0].getElementsByTagName('p')[0]
+
+// storico di tutte gli imput trasformati in lista
 let lista_da_calcolare = []
+
+// variabile per separare i numeri dalle operazioni
 let var_numero = ''
 
+
+// queste funzioni aggiungono il numero desiderato al display e aggiornano var_numero
 function aggiungi_0(){
     stringa_risultato.innerText += '0'
     var_numero += '0'
@@ -42,68 +50,130 @@ function aggiungi_9(){
     stringa_risultato.innerText += '9'
     var_numero += '9'
 }
-function aggiungi_diviso(){
-    stringa_risultato.innerText += '/'
-    lista_da_calcolare.push(parseInt(var_numero))
-    lista_da_calcolare.push('/')
-    var_numero = ''
-    console.log(lista_da_calcolare)
-}
-function aggiungi_per(){
-    stringa_risultato.innerText += '*'
-    lista_da_calcolare.push(parseInt(var_numero))
-    lista_da_calcolare.push('*')
-    var_numero = ''
-}
-function aggiungi_meno(){
-    stringa_risultato.innerText += '-'
-    lista_da_calcolare.push(parseInt(var_numero))
-    lista_da_calcolare.push('-')
-    var_numero = ''
-}
-function aggiungi_piu(){
-    stringa_risultato.innerText += '+'
-    lista_da_calcolare.push(parseInt(var_numero))
-    lista_da_calcolare.push('+')
-    var_numero = ''
-}
-function cancella(){
-}
 function aggiungi_punto(){
     stringa_risultato.innerText += '.'
     var_numero += '.'
 }
-function mostra_risultato(){
-    lista_da_calcolare.push(parseInt(var_numero))
+
+/* queste funzioni si occupano delle operiazioni e aggiungono alla
+lista di operazioni i numeri + l'operatore */
+function aggiungi_diviso(){
+    if (var_numero.includes('.')){
+        stringa_risultato.innerText += '/'
+        lista_da_calcolare.push(parseFloat(var_numero))
+        lista_da_calcolare.push('/')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }else{
+        stringa_risultato.innerText += '/'
+        lista_da_calcolare.push(parseInt(var_numero))
+        lista_da_calcolare.push('/')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }
+}
+function aggiungi_per(){
+    if (var_numero.includes('.')){
+        stringa_risultato.innerText += '*'
+        lista_da_calcolare.push(parseFloat(var_numero))
+        lista_da_calcolare.push('*')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }else{
+        stringa_risultato.innerText += '*'
+        lista_da_calcolare.push(parseInt(var_numero))
+        lista_da_calcolare.push('*')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }
+}
+function aggiungi_meno(){
+    if (var_numero.includes('.')){
+        stringa_risultato.innerText += '-'
+        lista_da_calcolare.push(parseFloat(var_numero))
+        lista_da_calcolare.push('-')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }else{
+        stringa_risultato.innerText += '-'
+        lista_da_calcolare.push(parseInt(var_numero))
+        lista_da_calcolare.push('-')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }
+}
+function aggiungi_piu(){
+    if (var_numero.includes('.')){
+        stringa_risultato.innerText += '+'
+        lista_da_calcolare.push(parseFloat(var_numero))
+        lista_da_calcolare.push('+')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }else{
+        stringa_risultato.innerText += '+'
+        lista_da_calcolare.push(parseInt(var_numero))
+        lista_da_calcolare.push('+')
+        var_numero = ''
+        console.log(lista_da_calcolare)
+    }
+}
+//funzione cancella
+function cancella(){
+    stringa_risultato.innerText = ''
+    lista_da_calcolare = []
     var_numero = ''
-    console.log(lista_da_calcolare)
-    while (lista_da_calcolare.length > 1) {
-        for (let index = 0; index < lista_da_calcolare.length; index++) {
-            if (lista_da_calcolare[index] == '*' || lista_da_calcolare[index] == '/'){
-                if (lista_da_calcolare[index] == '*'){
-                    lista_da_calcolare[index] = lista_da_calcolare[index-1] * lista_da_calcolare[index+1]
-                    lista_da_calcolare.splice(lista_da_calcolare[index-1],1)
-                    lista_da_calcolare.splice(lista_da_calcolare[index],1)
+}
 
-                }else{
-                    lista_da_calcolare[index] = lista_da_calcolare[index-1] / lista_da_calcolare[index+1]
-                    lista_da_calcolare.splice(lista_da_calcolare[index-1],1)
-                    lista_da_calcolare.splice(lista_da_calcolare[index],1)
-
-                }
-            }else{
-                if (lista_da_calcolare[index] == '+'){
-                    lista_da_calcolare[index] = lista_da_calcolare[index-1] + lista_da_calcolare[index+1]
-                    lista_da_calcolare.splice(lista_da_calcolare[index-1],1)
-                    lista_da_calcolare.splice(lista_da_calcolare[index],1)
-
-                }else{
-                    lista_da_calcolare[index] = lista_da_calcolare[index-1] - lista_da_calcolare[index+1]
-                    lista_da_calcolare.splice(lista_da_calcolare[index-1],1)
-                    lista_da_calcolare.splice(lista_da_calcolare[index],1)
-
+/*funzione risultato che itera la lista di operazioni e svolge prima le divisioni e moltiplicazioni 
+e poi le somme e sottrazioni, lasciando un unico valore nella lista che verra' passato al display
+come risultato  */
+function mostra_risultato(){
+    //l'ultimo numero non lo prende nessuno quindi lo fa risultato
+    if (var_numero.includes('.')){
+        lista_da_calcolare.push(parseInt(var_numero))
+        var_numero = ''
+    }else{
+        lista_da_calcolare.push(parseFloat(var_numero))
+        var_numero = ''
+    }
+    //mi copio la lista per non modificarla
+    let lista_risultato = [...lista_da_calcolare]
+    //1 perche' quello che mi rimarra' dopo tutte le operazioni e' un elemento, ovvero il risultato
+    while (lista_risultato.length > 1) {
+        //do priorita' alle moltiplicazioni e divisioni
+        if (lista_risultato.includes('*') || lista_risultato.includes('/') ){
+            for (let index = 0; index < lista_risultato.length; index++) {
+                if (lista_risultato[index] == '*'){
+                    let var1 = lista_risultato[index-1]
+                    let var2 = lista_risultato[index+1]
+                    lista_risultato.splice(index-1,3,var1*var2)
+                    //siccome non posso iterare su un array che si rimpicciolisce uso break
+                    break
+                }else if(lista_risultato[index] == '/'){
+                    let var1 = lista_risultato[index-1]
+                    let var2 = lista_risultato[index+1]
+                    lista_risultato.splice(index-1,3,var1/var2)
+                    //siccome non posso iterare su un array che si rimpicciolisce uso break
+                    break
                 }
             }
-        }
+        }else{
+            for (let index = 0; index < lista_risultato.length; index++) {
+                if (lista_risultato[index] == '+'){
+                    let var1 = lista_risultato[index-1]
+                    let var2 = lista_risultato[index+1]
+                    lista_risultato.splice(index-1,3,var1+var2)
+                    //siccome non posso iterare su un array che si rimpicciolisce uso break
+                    break
+                }else if(lista_risultato[index] == '-'){
+                    let var1 = lista_risultato[index-1]
+                    let var2 = lista_risultato[index+1]
+                    lista_risultato.splice(index-1,3,var1-var2)
+                    //siccome non posso iterare su un array che si rimpicciolisce uso break
+                    break
+                }
+            }
+        }          
     }
+    stringa_risultato.innerText = lista_risultato
 }
